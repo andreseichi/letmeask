a4905948a198abbb9138c2df013d84e6423f012e
André Tashiro <andre_seichi@hotmail.com>
2021-06-22 12:44:49 -0300

    importing the styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,17 +2,19 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
-        <img src= {illustrationImg} alt="Ilustração perguntas e respostas" />
+        <img src={illustrationImg} alt="Ilustração perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Responda as melhores perguntas de seus fãs</p>
       </aside>
 
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask Logo" />
           <button>
             <img src={googleIconImg} alt="Logo Google" />
@@ -20,15 +22,11 @@ export function Home() {
           </button>
           <div>ou entre em uma sala existente</div>
           <form>
-            <input 
-              type="text"
-              placeholder="Digite o código da sala"
-            />
-
+            <input type="text" placeholder="Digite o código da sala" />
             <button type="submit">Entrar na sala</button>
           </form>
         </div>
       </main>
     </div>
-  )
+  );
 }
